95e61211e1e407b7326b4e5647fbc28597281c2b
keshavp07-git <keshav.prajapati830@gmail.com>
2024-10-21 13:55:14 +0530

    Checked Session ID

--- src/main/java/com/keshav/SpringSecurity/HelloController.java
+++ src/main/java/com/keshav/SpringSecurity/HelloController.java
@@ -1,12 +1,13 @@
 package com.keshav.SpringSecurity;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String greet(){
-       return "Welcome to Site"; // Created this controller and secured by Spring Security Dependencies.
-    }
+    public String greet(HttpServletRequest request){
+       return "Welcome to Site  " + request.getSession().getId(); // Created this controller and secured by Spring Security Dependencies.
+    } // This How we can print session id on localhost page
 }
